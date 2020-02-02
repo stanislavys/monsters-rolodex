587080c5c01ddafb46323b31d3f51d6528bc8a3d
Stanislav Stoyanov <stanislavys@gmail.com>
2020-02-02 12:15:44 -0800

    Updates

---
+++ precache-manifest.a58d68d6fc000c74d6435abe13eae713.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65aefba9d20f7695926d85da4b2668fd",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "5aba2793eec5b1cec2e8",
+    "url": "/monsters-rolodex/static/css/main.101ef257.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aba2793eec5b1cec2e8",
+    "url": "/monsters-rolodex/static/js/main.4cda7041.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
